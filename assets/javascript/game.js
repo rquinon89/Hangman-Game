56b499fd93447ed13c15bc8183463101f349be48
Ricardo Quinones <Ricardo Quinones>
2017-07-11 17:59:47 -0500

    pathetic attempt to do hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+// i feel like i have to create an array for each trivia challenge
+
+var typeConspiracy [ "iluminati", "september eleven", "obama\s birth certificate", "jft assassination", "aliens", "area 51", "moon landing", "vaccinations" ]
+
+
+var Score=0;
+var questionIndex=0;
+var questionArray = []
+
+
+
+
+
+
+
+
+// need to include function renderQuestion to to the next one
+		//it would need to include a if else statement to know when to proceed to the next question 
+
+
+renderQuestion (): 
+updateScore ():
+
+
+document.onkeyup = function (event) {
+
+	if (questionindex ===questionArray.length){
+		return;
+	}
+}
+
+function updateScore () {
+	document.querySelector('#score').innerHTML = 'Score:' + score:
+
+}
+
+
+// need to define user input and include if else statements
+
+
+
+// need to include loop inorder for user to continue guessing
+function consoleInside (arr) {
+	for (var i = 0; i < arr.length; i++) {
+		console.log (arr[i]);
+	}
+	console.log (".....");
+}
